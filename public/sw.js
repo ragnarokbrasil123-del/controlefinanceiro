16a9da5298b5b538e7138c934347af9c55056988
ragnarokbrasil123-del <ragnarokbrasil123@gmail.com>
2026-06-14 23:22:43 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,14 @@
-self.addEventListener('install', (e) => {
+// Instalador do Motor Offline (Service Worker)
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (e) => {
-  // Apenas para agradar o Google e liberar a instalação
+// O navegador Brave/Chrome EXIGE que exista um interceptador de Fetch para liberar a instalação
+self.addEventListener('fetch', (event) => {
+  // Por enquanto, apenas deixa a internet funcionar normalmente
+  return;
 });
